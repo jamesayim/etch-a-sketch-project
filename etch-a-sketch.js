2a7c02083b28ff0d5ee58f28006a99cc5eb04296
jamesyim <theladjames@gmail.com>
2024-09-10 19:12:07 +0000

    updated ui and functionality of the sketch pad and add hover effect for all grids

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,45 +1,38 @@
 const divContainer = document.querySelector("#divContainer");
+const maxContainerSize = 800;
+const upperButton = document.querySelector(".upperButton");
 
 function createGrid(div) {
     for (let i = 0; i < div * div; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
-        divContainer.appendChild(square);
+        square.style.border = "1px solid black";
+        square.style.display = "flex";
+        square.style.flex = "1";
+        square.style.aspectRatio = "1/1";
+        square.style.minWidth = `${maxContainerSize / div}px`;
+        divContainer.append(square);
     }
 }
-
 createGrid(16);
 
-const upperButton = document.querySelector(".upperButton");
+function hoverEffect() {
+    let containerChildren = Array.from(divContainer.children);
+    containerChildren.forEach(element => {
+        element.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "black";
+        });
+    });
+}
+hoverEffect();
+
 upperButton.addEventListener("click", () => {
     let userInput = +prompt("Enter the number of grid(s) to be produced in the container:");
-    if (userInput >= 100) {
-        while (userInput >= 100) {
-            userInput = prompt(`Enter a number below 100.`);
+        while (userInput > 100) {
+            userInput = prompt(`Enter a number from 0 to 100.`);
         }
-    }
-    // calculating the size of squares to fit inside the container
-    const gap = 2;
-    const maxContainerSize = 800;
 
-    const squareSize = Math.floor((maxContainerSize - gap * (userInput - 1)) / userInput);
-    const containerSize = squareSize * userInput + gap * (userInput - 1);
-
-    // If the grid is too large, reduce the square size
-    if (squareSize < 10) { // Set a minimum size for squares
-        squareSize = 10;
-    }
-
-    // styling container
-    divContainer.style.display = 'flex';
-    divContainer.style.flexWrap = 'wrap';
-    divContainer.style.width = `${containerSize}px`;
-    divContainer.style.height = `${containerSize}px`;
-    divContainer.style.gap = `${gap}px`;
-
-    square.style.width = `${squareSize}px`;
-    square.style.height = `${squareSize}px`;
-
-    return createGrid(userInput);
+    divContainer.textContent = "";
+    createGrid(userInput);
+    hoverEffect();
 });
-
